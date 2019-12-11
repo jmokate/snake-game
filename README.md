bcc2943f8d571a2c3191759c01b8c9440e817e15
jmokate <jmokate@gmail.com>
2019-12-11 09:26:40 -0600

    working on readme. added html and js logos

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# snake-game
+# Retro Snake Game
+
+### Building this old school game has been a fun and challenging experience in building up my skills with Javascript. As an assignement from my mentorship program, this project trained me to flex a new muscle when it comes to working animation with the HTML Canvas in JS.
+
+#### This project was built using:
+
+## HTML 5 & Javascript ES6.
+
+![image of html5 and es6]
+
+### Run this game by downloading the index.html and main.js files, then open index.html in your browser.
--- main.js
+++ main.js
@@ -1,208 +1,195 @@
-const DEBUG = false;
+(function() {
+  const DEBUG = false;
 
-const UP = 38;
-const RIGHT = 39;
-const DOWN = 40;
-const LEFT = 37;
+  const UP = 38;
+  const RIGHT = 39;
+  const DOWN = 40;
+  const LEFT = 37;
 
-const snakeEdge = 20;
+  let snake = [{ x: 100, y: 100 }];
 
-let snake = [{ x: 100, y: 100 }];
+  let snakeSpeedX = 0;
+  let snakeSpeedY = 0;
 
-let snakeSpeedX = 0;
-let snakeSpeedY = 0;
+  let fps = 10;
 
-let fps = 10;
+  let score = 0;
+  const canvasColumn = 40;
+  const canvasRow = 30;
+  const gridSquareWidth = 20;
+  const gridSquareHeight = 20;
+  let grid = [];
 
-let score = 0;
-const canvasColumn = 40;
-const canvasRow = 30;
-const gridSquareWidth = 20;
-const gridSquareHeight = 20;
-let grid = [];
-
-for (c = 0; c < canvasColumn; c++) {
-  grid[c] = [];
-  for (r = 0; r < canvasRow; r++) {
-    grid[c][r] = { x: 0, y: 0 };
-  }
-}
-
-let randomX = gridSquareWidth * Math.floor(Math.random() * canvasColumn);
-let randomY = gridSquareHeight * Math.floor(Math.random() * canvasRow);
-
-let keyDirectionUp = true;
-let keyDirectionRight = true;
-let keyDirectionDown = true;
-let keyDirectionLeft = true;
-
-document.addEventListener("keydown", function(e) {
-  if (e.keyCode == UP) {
-    if (keyDirectionUp == true) {
-      snakeSpeedX = 0;
-      snakeSpeedY = 20;
-      keyDirectionDown = false;
-      keyDirectionRight = true;
-      keyDirectionLeft = true;
-    }
-  }
-  if (e.keyCode == RIGHT) {
-    if (keyDirectionRight == true) {
-      snakeSpeedY = 0;
-      snakeSpeedX = 20;
-      keyDirectionLeft = false;
-      keyDirectionUp = true;
-      keyDirectionDown = true;
-    }
-  }
-  if (e.keyCode == DOWN) {
-    if (keyDirectionDown == true) {
-      snakeSpeedX = 0;
-      snakeSpeedY = -20;
-      keyDirectionUp = false;
-      keyDirectionRight = true;
-      keyDirectionLeft = true;
-    }
-  }
-  if (e.keyCode == LEFT) {
-    if (keyDirectionLeft == true) {
-      snakeSpeedY = 0;
-      snakeSpeedX = -20;
-      keyDirectionRight = false;
-      keyDirectionUp = true;
-      keyDirectionDown = true;
-    }
-  }
-});
-
-window.onload = function() {
-  drawEverything();
-  //drawCanvas();
-
-  setInterval(function() {
-    requestAnimationFrame(drawEverything);
-  }, 1000 / fps);
-};
-
-function drawCanvas() {
-  canvas = document.querySelector("#gameCanvas");
-  canvas.width = 800;
-  canvas.height = 600;
-  canvasContext = canvas.getContext("2d");
-  colorRect(0, 0, canvas.width, canvas.height, "black");
-}
-
-function drawGrid() {
   for (c = 0; c < canvasColumn; c++) {
+    grid[c] = [];
     for (r = 0; r < canvasRow; r++) {
-      let gridX = c * gridSquareWidth;
-      let gridY = r * gridSquareHeight;
-      grid[c][r].x = gridX;
-      grid[c][r].y = gridY;
-      canvasContext.beginPath();
-      canvasContext.rect(gridX, gridY, gridSquareWidth, gridSquareHeight);
-
-      canvasContext.closePath();
+      grid[c][r] = { x: 0, y: 0 };
     }
   }
-}
 
-function moveSnake() {
-  let snakeCopy = [];
+  let randomX = gridSquareWidth * Math.floor(Math.random() * canvasColumn);
+  let randomY = gridSquareHeight * Math.floor(Math.random() * canvasRow);
 
-  snake.forEach(bodyPart => {
-    snakeCopy.push({ x: bodyPart.x, y: bodyPart.y });
+  let keyDirectionUp = true;
+  let keyDirectionRight = true;
+  let keyDirectionDown = true;
+  let keyDirectionLeft = true;
+
+  document.addEventListener("keydown", function(e) {
+    if (e.keyCode == UP) {
+      if (keyDirectionUp == true) {
+        snakeSpeedX = 0;
+        snakeSpeedY = 20;
+        keyDirectionDown = false;
+        keyDirectionRight = true;
+        keyDirectionLeft = true;
+      }
+    }
+    if (e.keyCode == RIGHT) {
+      if (keyDirectionRight == true) {
+        snakeSpeedY = 0;
+        snakeSpeedX = 20;
+        keyDirectionLeft = false;
+        keyDirectionUp = true;
+        keyDirectionDown = true;
+      }
+    }
+    if (e.keyCode == DOWN) {
+      if (keyDirectionDown == true) {
+        snakeSpeedX = 0;
+        snakeSpeedY = -20;
+        keyDirectionUp = false;
+        keyDirectionRight = true;
+        keyDirectionLeft = true;
+      }
+    }
+    if (e.keyCode == LEFT) {
+      if (keyDirectionLeft == true) {
+        snakeSpeedY = 0;
+        snakeSpeedX = -20;
+        keyDirectionRight = false;
+        keyDirectionUp = true;
+        keyDirectionDown = true;
+      }
+    }
   });
 
-  for (i = 0; i < snake.length; i++) {
-    if (i == 0) {
-      snake[i].y -= snakeSpeedY; //move up
-      snake[i].x += snakeSpeedX; //move right
-    } //else update current snake copy?
-    else {
-      snake[i].y = snakeCopy[i - 1].y;
-      snake[i].x = snakeCopy[i - 1].x;
-    }
-    colorRect(snake[i].x, snake[i].y, 20, 20, "green", "white");
+  window.onload = function() {
+    setInterval(function() {
+      requestAnimationFrame(drawEverything);
+    }, 1000 / fps);
+  };
+
+  function drawCanvas() {
+    canvas = document.querySelector("#gameCanvas");
+    canvas.width = 800;
+    canvas.height = 600;
+    canvasContext = canvas.getContext("2d");
+    colorRect(0, 0, canvas.width, canvas.height, "black");
   }
 
-  for (i = 1; i < snake.length; i++) {
-    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+  function drawGrid() {
+    for (c = 0; c < canvasColumn; c++) {
+      for (r = 0; r < canvasRow; r++) {
+        let gridX = c * gridSquareWidth;
+        let gridY = r * gridSquareHeight;
+        grid[c][r].x = gridX;
+        grid[c][r].y = gridY;
+        canvasContext.beginPath();
+        canvasContext.rect(gridX, gridY, gridSquareWidth, gridSquareHeight);
+        canvasContext.closePath();
+      }
+    }
+  }
+
+  function moveSnake() {
+    let snakeCopy = [];
+
+    snake.forEach(bodyPart => {
+      snakeCopy.push({ x: bodyPart.x, y: bodyPart.y });
+    });
+
+    for (i = 0; i < snake.length; i++) {
+      if (i == 0) {
+        snake[i].y -= snakeSpeedY;
+        snake[i].x += snakeSpeedX;
+      } else {
+        snake[i].y = snakeCopy[i - 1].y;
+        snake[i].x = snakeCopy[i - 1].x;
+      }
+      colorRect(snake[i].x, snake[i].y, 20, 20, "green", "white");
+    }
+
+    for (i = 1; i < snake.length; i++) {
+      if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+        gameOver();
+      }
+    }
+
+    if (snake[0].x > canvas.width - 20) {
       gameOver();
     }
-  }
-
-  if (snake[0].x > canvas.width - 20) {
-    gameOver();
-  }
-  if (snake[0].x < 0) {
-    gameOver();
-  }
-  if (snake[0].y > canvas.height - 20) {
-    gameOver();
-  }
-  if (snake[0].y < 0) {
-    gameOver();
-  }
-
-  if (snake[0].x === randomX && snake[0].y === randomY) {
-    console.log("score", score);
-    score++;
-    newApple();
-    snake.push({ x: snake.x, y: snake.y });
-    console.log(snake);
-  }
-}
-
-function newApple() {
-  randomX = gridSquareWidth * Math.floor(Math.random() * canvasColumn);
-  randomY = gridSquareHeight * Math.floor(Math.random() * canvasRow);
-
-  snake.forEach(bodyPart => {
-    if (randomX !== bodyPart.x && randomY !== bodyPart.y) {
-      colorRect(randomX, randomY, 20, 20, "red");
-    } else if (randomX == bodyPart.x && randomY == bodyPart.y) {
-      randomX = gridSquareWidth * Math.floor(Math.random() * canvasColumn);
-      randomY = gridSquareHeight * Math.floor(Math.random() * canvasRow);
+    if (snake[0].x < 0) {
+      gameOver();
     }
-  });
+    if (snake[0].y > canvas.height - 20) {
+      gameOver();
+    }
+    if (snake[0].y < 0) {
+      gameOver();
+    }
 
-  // if (randomX && randomY !== snake.x && snake.y) {
-  //   appleSpawningOnSnake = false;
-  //   colorRect(randomX, randomY, 20, 20, "red");
-  // }
-  //console.log(appleSpawningOnSnake);
-}
+    if (snake[0].x === randomX && snake[0].y === randomY) {
+      console.log("score", score);
+      score++;
+      newApple();
+      snake.push({ x: snake.x, y: snake.y });
+    }
+  }
 
-function gameOver() {
-  document.location.reload();
-  alert("you died :(");
-}
+  function newApple() {
+    randomX = gridSquareWidth * Math.floor(Math.random() * canvasColumn);
+    randomY = gridSquareHeight * Math.floor(Math.random() * canvasRow);
 
-function drawEverything() {
-  canvas = document.querySelector("#gameCanvas");
-  canvas.width = 800;
-  canvas.height = 600;
-  canvasContext = canvas.getContext("2d");
-  //draws game canvas
-  //colorRect(0, 0, canvas.width, canvas.height, "black");
-  drawCanvas();
-  drawGrid();
-  moveSnake();
-  apple = colorRect(randomX, randomY, 20, 20, "red");
-  drawScore();
-}
+    snake.forEach(bodyPart => {
+      if (randomX !== bodyPart.x && randomY !== bodyPart.y) {
+        colorRect(randomX, randomY, 20, 20, "red");
+      } else if (randomX == bodyPart.x && randomY == bodyPart.y) {
+        randomX = gridSquareWidth * Math.floor(Math.random() * canvasColumn);
+        randomY = gridSquareHeight * Math.floor(Math.random() * canvasRow);
+      }
+    });
+  }
 
-function drawScore() {
-  canvasContext.font = "16px arial";
-  canvasContext.fillStyle = "white";
-  canvasContext.fillText("Score: " + score, 360, 20);
-}
+  function gameOver() {
+    document.location.reload();
+    alert("you died :(");
+  }
 
-//this function fills in color for everything
-function colorRect(leftX, topy, width, height, drawColor, strokeColor) {
-  canvasContext.fillStyle = drawColor;
-  canvasContext.strokeStyle = strokeColor;
-  canvasContext.strokeRect(leftX, topy, width, height);
-  canvasContext.fillRect(leftX, topy, width, height);
-}
+  function drawEverything() {
+    canvas = document.querySelector("#gameCanvas");
+    canvas.width = 800;
+    canvas.height = 600;
+    canvasContext = canvas.getContext("2d");
+
+    drawCanvas();
+    drawGrid();
+    moveSnake();
+    drawScore();
+    apple = colorRect(randomX, randomY, 20, 20, "red");
+  }
+
+  function drawScore() {
+    canvasContext.font = "16px arial";
+    canvasContext.fillStyle = "white";
+    canvasContext.fillText("Score: " + score, 360, 20);
+  }
+
+  //fills in color for everything
+  function colorRect(leftX, topy, width, height, drawColor, strokeColor) {
+    canvasContext.fillStyle = drawColor;
+    canvasContext.strokeStyle = strokeColor;
+    canvasContext.strokeRect(leftX, topy, width, height);
+    canvasContext.fillRect(leftX, topy, width, height);
+  }
+})();
